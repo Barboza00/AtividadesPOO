1e312949a723d2867167506ac549c472c2409b1e
Barboza00 <adonisbarbosa2003@gmail.com>
2026-04-04 15:08:21 -0400

    Atividade 2

--- src/Carro.java
+++ src/Carro.java
@@ -10,11 +10,24 @@ public class Carro {
         this.anoFabricacao = anoFabricacao;
     }
 
-public static void main(String[] args) {
-    Carro carro1 = new Carro("Honda", "Civic", 2025);
-    Carro carro2 = new Carro("Volkswagem", "Jetta", 2025);
+    public void exibir(){
+        System.out.println("Carro: " + marca + " " + modelo + " - Ano: " + anoFabricacao);
+    }
 
-    System.out.println("Carro 1: " + carro1.marca + " " + carro1.modelo + " " + carro1.anoFabricacao);
-    System.out.println("Carro 2: " + carro2.marca + " " + carro2.modelo + " " + carro2.anoFabricacao);
+    public int idadeDoCarro(){
+        return 2026 - anoFabricacao;
+    }
+
+public static void main(String[] args) {
+    Carro carro1 = new Carro("Honda", "Civic", 2016);
+    Carro carro2 = new Carro("Volkswagem", "Jetta", 2010);
+
+    //Executa ação pois é um metodo "void".
+    carro1.exibir();
+    carro2.exibir();
+
+    //Devolve valor pois ele retorna um "int".
+    System.out.println("Idade do Honda: " + carro1.idadeDoCarro() + " anos.");
+    System.out.println("Idade do Honda: " + carro2.idadeDoCarro() + " anos.");
     }
 }
